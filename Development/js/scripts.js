8321dab79857626215cc7f9a6a0cddca1300261f
فوز فارس فه <66726084+fouzalmutairi@users.noreply.github.com>
2020-07-02 16:44:03 +0300

    done

---
+++ Development/js/scripts.js
@@ -0,0 +1,79 @@
+let name=["sarah,fouz,wasimya"];
+name.foreach(logger);
+function logger (value){
+    console.log(value)
+}
+//
+let summerdeg=[47,48.53.55];
+console.log(summerdeg.filter(my function));
+
+function my function name(params) {
+    
+} (x){
+    return x >50
+}
+//
+let books=[
+    {
+        name:"book1",
+        ID1,
+    },
+{
+    name:"book2",
+    ID:2,
+},
+
+{
+name:"book3",
+},
+
+{
+
+    name:"blook4",
+    ID:4,
+},
+
+]
+getbookID(books,4)
+function getboosbyID(books,booksID){
+    for (i of books){
+        if (i.ID==ID){
+            console.log("متوفر");
+        }
+    }
+}
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
